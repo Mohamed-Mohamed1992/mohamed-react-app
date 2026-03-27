19620aecad688611a3d4f4dd357b600418172e34
Mohamed Mohamed <250026672+Mohamed-Mohamed1992@users.noreply.github.com>
2026-03-27 20:56:44 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Greeting from "./components/Greeting";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Greeting name="Musa" surname="Mohamed"/>
+      <Header />
+      <Header />
+      <Header />
+    </>
+  );
+}
+
+export default App;
