8ffb2f00fcf150b730d6473da19c895e3a1c8f27
Mohamed Mohamed <250026672+Mohamed-Mohamed1992@users.noreply.github.com>
2026-03-27 22:24:29 +0000

    Add new counter component, add styling to counter, greeting, header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
+import Counter from "./components/counter";
 import Greeting from "./components/Greeting";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Greeting name="Musa" surname="Mohamed"/>
-      <Header />
-      <Header />
-      <Header />
+      <Counter />
     </>
   );
 }
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,15 +1,14 @@
 import { Fragment } from "react";
-
 function Greeting(props) {
   return (
-    <>
+    <div style={{ backgroundColor: "black", color: "white" }}>
       <h1>
         Hello, {props.name} {props.surname}!
       </h1>
       <h2>
         This text was not there
       </h2>
-    </>
+    </div>
   );
 }
 
